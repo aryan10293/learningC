784333424aefe5948d8d34767c3a6fa85b358b20
aondrej ryan <aondrejryan11@gmail.com>
2023-09-16 23:11:26 -0700

    learning c slowly but surely

--- IDK.c
+++ IDK.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("fuck me dude");
-
+    char tikTacToeOutcomes[][20] = {
+        "rock",
+        "paper",
+        "scissors"};
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("Outcome %d: %s\n", i + 1, tikTacToeOutcomes[i]);
+    // }
+    int arraySize = sizeof(tikTacToeOutcomes) / sizeof(tikTacToeOutcomes[0]);
+    printf("Array size: %d\n", arraySize);
     return 0;
 }
