6fb01db6c5ba0417bd08e01ae163246e4004252b
aondrej ryan <aondrejryan11@gmail.com>
2023-09-18 23:58:49 -0700

    learning c

--- IDK.c
+++ IDK.c
@@ -4,15 +4,20 @@
 
 int main()
 {
-    char tikTacToeOutcomes[][20] = {
-        "rock",
-        "paper",
-        "scissors"};
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     printf("Outcome %d: %s\n", i + 1, tikTacToeOutcomes[i]);
-    // }
-    int arraySize = sizeof(tikTacToeOutcomes) / sizeof(tikTacToeOutcomes[0]);
-    printf("Array size: %d\n", arraySize);
+    // char tikTacToeOutcomes[][20] = {
+    //     "rock",
+    //     "paper",
+    //     "scissors"};
+    // // for (int i = 0; i < 3; i++)
+    // // {
+    // //     printf("Outcome %d: %s\n", i + 1, tikTacToeOutcomes[i]);
+    // // }
+    // int arraySize = sizeof(tikTacToeOutcomes) / sizeof(tikTacToeOutcomes[0]);
+    // printf("Array size: %d\n", arraySize);
+
+    char favPlayer[50];
+    printf("whos the best basketball player ever?\n");
+    // scanf("&s/n", &favPlayer);
+    printf("your favorite player is %s", favPlayer);
     return 0;
 }
