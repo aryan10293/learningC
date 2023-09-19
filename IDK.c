9ce1acf6d2b176825478bc9717b99db8e9cb9f61
aondrej ryan <aondrejryan11@gmail.com>
2023-09-19 12:45:30 -0700

    learning to use numbers in c

--- IDK.c
+++ IDK.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
 
 // int main()
 // {
@@ -22,15 +23,57 @@
 //     printf("your favorite player is %s", favPlayer);
 //     return 0;
 // }
+int findCircumfrence()
+{
+    // char favPlayer[50];
+    // printf("Who's the your favorite basketball player?\n");
+
+    // // Pass favPlayer without the '&' symbol
+    // fgets(favPlayer, 50, stdin);
+    // favPlayer[strcspn(favPlayer, "\n")] = '\0';
+    // printf("Your favorite player is %s\n", favPlayer);
+    // printf("%lu\n", strlen(favPlayer));
+    // return 0;
+
+    float circumfence;
+    int radius;
+
+    printf("whats the radius of your circle ");
+
+    // scanf("%d", &radius);
+    if (scanf("%d", &radius) == 1)
+    {
+        circumfence = 2 * 3.14159 * radius;
+        printf("im working on something %.2f\n", circumfence);
+    }
+    else
+    {
+        printf("just make sure you enter a number next time\n");
+    };
+
+    // circumfence = 2 * 3.14159 * radius;
+    // printf("the radius you entered was %d \n", radius);
+    // printf("im working on something %.2f\n", circumfence);
+    return 0;
+}
 int main()
 {
-    char favPlayer[50];
-    printf("Who's the best basketball player ever?\n");
+    double theAnswer;
+    double oneSideOfThetriangle;
+    double otherSideOfThetriangle;
+    double right;
+    double left;
 
-    // Pass favPlayer without the '&' symbol
-    fgets(favPlayer, 50, stdin);
-    favPlayer[strcspn(favPlayer, "\n")] = '\0';
-    printf("Your favorite player is %s\n", favPlayer);
+    printf("whats one side of the triangle? \n");
+    scanf("%lf", &oneSideOfThetriangle);
 
+    printf("whats the other side of the triangle? \n");
+    scanf("%lf", &otherSideOfThetriangle);
+
+    right = pow(otherSideOfThetriangle, 2);
+    left = pow(oneSideOfThetriangle, 2);
+
+    theAnswer = sqrt(right + left);
+    printf("%.2lf \n", theAnswer);
     return 0;
 }
